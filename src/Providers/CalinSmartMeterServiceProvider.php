59798f9e8532bd266d425436c8ebf14c14bb4db5
93Kamuran <enginkamuran@gmail.com>
2021-04-07 11:43:01 +0300

    Package updating feature added.

--- src/Providers/CalinSmartMeterServiceProvider.php
+++ src/Providers/CalinSmartMeterServiceProvider.php
@@ -2,20 +2,14 @@
 
 namespace Inensus\CalinSmartMeter\Providers;
 
-use App\Models\MainSettings;
-use App\Models\Manufacturer;
-use App\Models\Meter\MeterParameter;
-use App\Models\Transaction\Transaction;
-use GuzzleHttp\Client;
 use Illuminate\Filesystem\Filesystem;
 use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\ServiceProvider;
 use Inensus\CalinSmartMeter\CalinSmartMeterApi;
 use Inensus\CalinSmartMeter\Console\Commands\InstallPackage;
-use Inensus\CalinSmartMeter\Helpers\ApiHelpers;
-use Inensus\CalinSmartMeter\Http\Requests\CalinSmartMeterApiRequests;
-use Inensus\CalinSmartMeter\Models\CalinSmartCredential;
-use Inensus\CalinSmartMeter\Models\CalinSmartTransaction;
+use Inensus\CalinSmartMeter\Console\Commands\UpdatePackage;
+
 
 class CalinSmartMeterServiceProvider extends ServiceProvider
 {
@@ -26,7 +20,7 @@ class CalinSmartMeterServiceProvider extends ServiceProvider
             $this->publishConfigFiles();
             $this->publishVueFiles();
             $this->publishMigrations($filesystem);
-            $this->commands([InstallPackage::class]);
+            $this->commands([InstallPackage::class,UpdatePackage::class]);
         }
     }
 
@@ -35,27 +29,7 @@ class CalinSmartMeterServiceProvider extends ServiceProvider
         $this->mergeConfigFrom(__DIR__ . '/../../config/calin-smart-meter.php', 'calin-smart-meter');
         $this->app->register(EventServiceProvider::class);
         $this->app->register(ObserverServiceProvider::class);
-        $this->app->bind('CalinSmartMeterApi', function () {
-            $client = new Client();
-            $meterParameter = new MeterParameter();
-            $transaction = new Transaction();
-            $calinSmartTransaction = new CalinSmartTransaction();
-            $mainSettings = new MainSettings();
-            $calinSmartCredential = new CalinSmartCredential();
-            $manufacturer = new Manufacturer();
-            $apiHelpers = new ApiHelpers($manufacturer);
-            $apiRequests = new CalinSmartMeterApiRequests($client, $apiHelpers, $calinSmartCredential);
-            return new CalinSmartMeterApi(
-                $client,
-                $meterParameter,
-                $calinSmartTransaction,
-                $transaction,
-                $mainSettings,
-                $calinSmartCredential,
-                $apiRequests,
-                $apiHelpers
-            );
-        });
+        $this->app->bind('CalinSmartMeterApi', CalinSmartMeterApi::class);
     }
     public function publishConfigFiles()
     {
@@ -83,6 +57,14 @@ class CalinSmartMeterServiceProvider extends ServiceProvider
         $timestamp = date('Y_m_d_His');
         return Collection::make($this->app->databasePath() . DIRECTORY_SEPARATOR . 'migrations' . DIRECTORY_SEPARATOR)
             ->flatMap(function ($path) use ($filesystem) {
+                if (count($filesystem->glob($path . '*_create_calin_smart_tables.php'))) {
+                    $file = $filesystem->glob($path . '*_create_calin_smart_tables.php')[0];
+
+                    file_put_contents($file, file_get_contents(__DIR__ . '/../../database/migrations/create_calin_smart_tables.php.stub'));
+                    DB::table('migrations')
+                        ->where('migration',substr(explode("/migrations/", $file)[1], 0, -4))
+                        ->delete();
+                }
                 return $filesystem->glob($path . '*_create_calin_smart_tables.php');
             })->push($this->app->databasePath() . "/migrations/{$timestamp}_create_calin_smart_tables.php")
             ->first();
