6672a1ea91423de6bb0bdb897392864a81028ea3
93Kamuran <enginkamuran@gmail.com>
2021-04-08 15:11:42 +0300

    MenuItemService.php refactored.

--- src/Services/MenuItemService.php
+++ src/Services/MenuItemService.php
@@ -2,23 +2,13 @@
 
 namespace Inensus\CalinSmartMeter\Services;
 
-use App\Models\MenuItems;
 
 class MenuItemService
 {
 
-    private $menuItems;
-
-    public function __construct(MenuItems $menuItems)
-    {
-        $this->menuItems = $menuItems;
-    }
     public function createMenuItems()
     {
-        $menuItem = $this->menuItems->newQuery()->where('name', 'Calin Smart Meter')->first();
-        if ($menuItem) {
-            return [];
-        }
+
         $menuItem = [
             'name' => 'Calin Smart Meter',
             'url_slug' => '',
